f5c86f9bca3765e8ff16935e8292032cd63d3b1a
riven <4771215@qq.com>
2021-07-21 18:46:22 +0800

    impl of sugarbox

--- main.ts
+++ main.ts
@@ -227,6 +227,43 @@ namespace Sugarmodule {
 //% color="#90B7FB" weight=10 icon="\uf8ff"
 //% groups='["Actuators", "Encoded Motor", "Dual Encoded Motor", "Audio"]'
 namespace SugarBox {
+
+  const MODE_IDLE   = 0x0
+  const MODE_SPEED  = 0x1
+  const MODE_HOLD   = 0x2
+  const MODE_RUNPOS = 0x4 // there is some end position
+  const MODE_ABSOLU = 0x8
+  const MODE_DELAY  = 0x10
+  const MODE_STUCK  = 0x20
+  const MODE_RAMPDN = 0x40
+  const MODE_TOSTOP = 0x80
+
+  enum E_PARAM {
+    Kp = 1,
+    Ki = 2,
+    Kd = 3,
+    POSITION = 4,
+    SPEED = 5,
+    PULSE = 6,
+    GEARRATIO = 7,
+    LOOPMS = 8,
+    Kp_hold = 9,
+    Ki_hold = 10,
+    Kd_hold = 11,
+    RAMPDOWN = 12
+  }
+
+
+
+  const SGBOX_ADDR = 29
+  const REG_SERVO = 0x1
+  const REG_MOTOR = 0x2
+  const REG_PIDRUN = 0x3
+  const REG_PIDSAV = 0x4
+  const REG_PIDLOD = 0x5
+  const REG_DUALRUN = 0x6
+  const REG_VOLTAG = 0x8
+  const REG_PIDRESET = 0x9
   export enum MPort{
     M1A = 1,
     M1B = 2,
@@ -243,109 +280,210 @@ namespace SugarBox {
 
   export enum EPort {
     EM1 = 1,
-    EM2 = 2
+    EM2 = 2,
+  }
+
+  export enum DSetup {
+    //% block="M1A@Left"
+    LR = 1,
+    //% block="M2A@Left"
+    RL = 2
+  }
+
+  function _i2cWriteBH(reg: number, index: number, value: number){
+    const buff = pins.createBuffer(4) // reg, int16
+    buff[0] = reg
+    buff[1] = index
+    buff.setNumber(NumberFormat.Int16LE, 2, value)
+    pins.i2cWriteBuffer(SGBOX_ADDR, buff)
+  }
+
+  function _i2cReadF (port: EPort, p: E_PARAM): number {
+    let _reg = port == EPort.EM1 ? 0x60 : 0x70
+    _reg |= p
+    return i2cread(SGBOX_ADDR, _reg, 4).getNumber(NumberFormat.Float32LE, 0)
   }
 
   //% blockId=battery block="Battery Voltage"
   //% group="Dual Encoded Motor" weight=50
   export function battery(): number {
-    return 3.72
+    return i2cread(SGBOX_ADDR,REG_VOLTAG,4).getNumber(NumberFormat.Float32LE,0)
   }
 
   //% blockId=motor_spd block="Motor|%motor Speed %spd"
   //% group="Actuators" weight=49
   export function motorSpd(port: MPort, speed: number) {
-    
+    _i2cWriteBH(REG_MOTOR,port,speed)
   }
 
   //% blockId=motor_stop block="Motor|%motor Stop"
   //% group="Actuators" weight=49
   export function motorStop(port: MPort) {
-    
+    _i2cWriteBH(REG_MOTOR,port,0)
   }
 
   //% blockId=servo2kg block="Servo|%motor Angle %angle"
   //% group="Actuators" weight=48
   export function servo2kg(port: SPort, angle: number) {
-    
+    const us =  Math.floor(angle*200/36+500) // 2kg
+    servoPulse(port, us)
   }
 
   //% blockId=servo_pulse block="Servo|%motor Pulse %us us"
   //% group="Actuators" weight=48
   export function servoPulse(port: SPort, us: number) {
-    
+    _i2cWriteBH(REG_SERVO,port,us)
+  }
+
+  function _emotorReset(port: EPort){
+    i2cwrite(SGBOX_ADDR,REG_PIDRESET,[port,0])
+  }
+
+  function _pidRun(port: EPort, mode: number, speed: number, param2: number, wait: boolean = true){
+    const buf = pins.createBuffer(10) // B,B,f,f
+    buf[0] = port
+    buf[1] = mode
+    buf.setNumber(NumberFormat.Float32LE, 2, speed)
+    buf.setNumber(NumberFormat.Float32LE, 6, param2)
+    pins.i2cWriteBuffer(SGBOX_ADDR, buf)
+    if (wait){
+      let _reg = port == EPort.EM1 ? 0x60 : 0x70
+      while (mode != 0){
+        mode = i2cread(SGBOX_ADDR,_reg,1)[0]
+        if (mode & MODE_STUCK){
+          _emotorReset(port)
+          return -1;
+        }
+        basic.pause(200)
+      }
+    }
   }
 
   //% blockId=enc_init block="Encoder Motor|%motor Init"
   //% group="Encoded Motor" weight=40
-  export function encMotorInit(port: SPort) {
-    
+  export function encMotorInit(port: EPort) {
+    _emotorReset(port)
   } 
 
   //% blockId=enc_rpm_set block="EMotor %motor run %spd RPM"
   //% group="Encoded Motor" weight=39
-  export function eMotorSetRpm(port: SPort, spd: number) {
-    
+  export function eMotorSetRpm(port: EPort, spd: number) {
+    spd /= 60 // from rpm to rnd per sec
+    _pidRun(port,MODE_SPEED,spd,0,false)
   }
 
   //% blockId=enc_rpm_get block="EMotor %motor get Speed(RPM)"
   //% group="Encoded Motor" weight=38
-  export function eMotorGetRpm(port: SPort): number {
-    return 120;
+  export function eMotorGetRpm(port: EPort): number {
+    return _i2cReadF(port, E_PARAM.SPEED)
   }
 
   //% blockId=enc_goto block="EMotor %motor Goto degree %degree speed %rpm RPM"
   //% group="Encoded Motor" weight=36
-  export function eMotorGoto(port: SPort, degree: number, rpm: number) {
-    
+  export function eMotorGoto(port: EPort, degree: number, rpm: number) {
+    // speed to RPSec, degree to round
+    const speed = rpm/60
+    const rnd = degree/360
+    _pidRun(port, MODE_SPEED | MODE_RUNPOS, speed, rnd, true)
   }
 
   //% blockId=enc_position block="EMotor %motor Position degree"
   //% group="Encoded Motor" weight=36
-  export function eMotorPos(port: SPort): number {
-    return 0;
+  export function eMotorPos(port: EPort): number {
+    return _i2cReadF(port, E_PARAM.POSITION)
   }
 
   //% blockId=enc_set_pos block="EMotor %motor To Position %degree"
   //% group="Encoded Motor" weight=36
-  export function eMotorSetPos(port: SPort) {
-    
+  export function eMotorSetPos(port: EPort, degree: number) {
+    // speed to RPSec, degree to round
+    const speed = 2 // 120 rpm
+    const rnd = degree/360
+    const mode = MODE_SPEED | MODE_RUNPOS | MODE_ABSOLU
+    _pidRun(port,mode, speed, rnd, true)
   }
 
   //% blockId=enc_move_deg block="EMotor %motor Move By Degree %degree, speed %speed RPM"
   //% group="Encoded Motor" weight=34
-  export function eMotorMoveDeg(port: SPort, degree: number, speed: number) {
-    
+  export function eMotorMoveDeg(port: EPort, degree: number, speed: number) {
+    eMotorGoto(port, degree, speed)
   }
 
   //% blockId=enc_move_rnd block="EMotor %motor Move Round %rnd, speed %speed RPM"
   //% group="Encoded Motor" weight=34
-  export function eMotorMoveRnd(port: SPort, rnd: number, speed: number) {
-    
+  export function eMotorMoveRnd(port: EPort, rnd: number, speed: number) {
+    eMotorGoto(port, rnd*360, speed)
   }
 
   //% blockId=enc_move_delay block="EMotor %motor Move Delayed %t sec, speed %speed RPM"
   //% group="Encoded Motor" weight=34
-  export function eMotorMoveDelayed(port: SPort, rnd: number, speed: number) {
-    
+  export function eMotorMoveDelayed(port: EPort, t: number, speed: number) {
+    const mode = MODE_SPEED | MODE_DELAY
+    _pidRun(port,mode, speed, t*1000, true)
+  }
+
+  // initial params in CM
+  let _R = 6
+  let _W = 12
+  let _Setup = 1
+
+  const MODE_RUN    = 0x1
+  const MODE_TURN   = 0x2
+
+  function _dmotorReset (){
+    i2cwrite(SGBOX_ADDR, REG_PIDRESET, [3, _Setup])
+  }
+
+  function _dualMotorRun(mode: number, v: number, w: number, rnd: number, wait: boolean = true){
+    const buf = pins.createBuffer(10) // B,f,f,f
+    buf[0] = mode
+    buf.setNumber(NumberFormat.Float32LE, 1, v) // forward linear speed
+    buf.setNumber(NumberFormat.Float32LE, 5, w) // angular speed
+    buf.setNumber(NumberFormat.Float32LE, 9, rnd)
+    pins.i2cWriteBuffer(SGBOX_ADDR, buf)
+    if (wait){
+      let _reg = 0x80
+      while (mode != 0){
+        mode = i2cread(SGBOX_ADDR,_reg,1)[0]
+        if (mode & MODE_STUCK){
+          _dmotorReset()
+          return -1;
+        }
+        basic.pause(200)
+      }
+    }
   }
 
   //% blockId=denc_init block="Dual encoded motor init|wheel diameter(cm) %diameter|track width(cm) %width|left %ml right %mr|inversed %inversed"
   //% group="Dual Encoded Motor" weight=30
-  export function dualMotorInit(diameter: number,width: number,ml: EPort,mr: EPort,inversed: boolean) {
-    
+  export function dualMotorInit(diameter: number,width: number,setup:DSetup,inversed: boolean) {
+    _Setup = 0
+    _R = diameter
+    _W = width
+    if (setup == DSetup.RL){
+      _Setup |= 0x1
+    }
+    if (inversed){
+      _Setup |= 0x2
+    }
+    _dmotorReset()
   }
 
   //% blockId=denc_move block="Move %distance cm, speed %speed cm/s"
   //% group="Dual Encoded Motor" weight=28
-  export function dualMotorMove(diameter: number,speed: number) {
-    
+  export function dualMotorMove(distance: number,speed: number) {
+    const rnd = distance/(Math.PI*_R)
+    speed = speed/(Math.PI*_R)
+    _dualMotorRun(MODE_RUN, speed, 0, rnd)
   }
 
   //% blockId=denc_turn block="Turn degree %degree, speed %w degree/s, forward speed %v cm/s"
   //% group="Dual Encoded Motor" weight=27
-  export function dualMotorTurn(diameter: number,w: number, v: number) {
-    
+  export function dualMotorTurn(degree: number,w: number, v: number) {
+    speed = speed/(Math.PI*_R) // in round/s
+    const diff = w*_W/_R/360 // wheel difference
+    const rnd = 2*(degree/w)*diff
+    _dualMotorRun(MODE_TURN, speed, diff, rnd)
   }
 
 }
